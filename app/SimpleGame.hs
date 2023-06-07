a6fc78d76075b99a8e86cb6b36759109c42b0467
madiestic <madjestic13@gmail.com>
2023-06-07 23:56:50 +0200

    simplifying a bit

--- app/SimpleGame.hs
+++ app/SimpleGame.hs
@@ -20,11 +20,11 @@ initGame =
   Game { tick = -1 }
 
 game :: MSF (ReaderT DTime (ReaderT Game IO)) () Game
-game = arrM (\() -> (lift . lift) initGameIO)
+game = arrM (\() -> (lift . lift) updateGame)
 
-initGameIO :: IO Game
-initGameIO = do
-  print "initGame"
+updateGame :: IO Game
+updateGame = do
+  print "updateGame"
   return $ initGame { tick = 1 }
   
 render :: Game -> IO ()
@@ -32,10 +32,6 @@ render g = do
   print g
   threadDelay 1000000
   return ()
-
-foo :: MSF (ReaderT DTime (ReaderT Game IO)) ()          Game
-    -> MSF IO                                (DTime, ()) Game
-foo = undefined
 
 animate :: MSF (ReaderT DTime (ReaderT Game IO)) () Game -> IO ()  
 animate sf =
@@ -45,11 +41,6 @@ animate sf =
     sfIO    = runReaderS (runReaderS sf)        :: MSF IO (Game, (DTime, ()))  Game
     actuate = arrM render                       :: MSF IO  Game               ()
   
-runGame :: Window -> IO ()
-runGame window = do
-  renderer <- createRenderer window (-1) defaultRenderer
-  animate game
-
 main :: IO ()
 main = do
   let
@@ -62,7 +53,8 @@ main = do
   _ <- setMouseLocationMode RelativeLocation
   _ <- warpMouse (WarpInWindow window) (P (V2 (resX`div`2) (resY`div`2)))
   _ <- cursorVisible $= True
-  
-  runGame window
+
+  renderer <- createRenderer window (-1) defaultRenderer
+  animate game
   putStrLn "Exiting Game"
   
